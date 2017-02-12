a1322eb96ad052e87a12286ce2f78ffff8ff80ca
juan <chavezvasquezjuan@gmail.com>
2017-02-12 13:55:26 -0500

    url edit

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.posts_home),
     url(r'^index/', views.post_list, name = 'index'),
     url(r'^create/', views.post_create, name = 'create'),
-    url(r'^edit/(?P<id>\d+)/$', views.post_edit, name = 'edit'),
+    url(r'^edit/(?P<id>\w+)/$', views.post_edit, name = 'edit'),
+    url(r'^delete/(?P<id>\w+)/$', views.post_delete, name = 'delete'),
 ]
