88b6967e781fb79ef3c65faa3c410b06135a9b6f
juan <chavezvasquezjuan@gmail.com>
2017-02-12 13:58:34 -0500

    add def delete and edit post

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.http import HttpResponse
 
 from .models import Post
@@ -8,7 +8,6 @@ def posts_home(request):
     return HttpResponse("<h1>sdfd</h1>")
 
 def post_list(request):
-    instance = get_object_or_404(Post, id = 2)
     posts = Post.objects.all()
 
     context = {
@@ -18,7 +17,19 @@ def post_list(request):
     return render(request, 'post/index.html', context)
 
 def post_create(request):
-    
+
+    if request.method == 'POST':
+        title   = request.POST["title"]
+        content = request.POST["content"]
+
+        post = Post(
+            title   = title,
+            content = content
+        )
+        post.save()
+
+        return HttpResponseRedirect('/posts/index/')
+
     context = {
 
     }
@@ -26,13 +37,28 @@ def post_create(request):
     return render(request, 'post/create.html', context)
 
 def post_edit(request, id):
+    
     post = get_object_or_404(Post, id = id)
     
+    if request.method == 'POST':
+        title   = request.POST["title"]
+        content = request.POST["content"]
+        
+        post.title   = title
+        post.content = content
+        post.save()
+
+        return HttpResponseRedirect('/posts/index/')
+
     context = {
         'post': post,
     }
 
     return render(request, 'post/edit.html', context)
 
-def post_delete(args):
-    return HttpResponse("<h1>delete</h1>")
+def post_delete(render, id):
+    
+    post = get_object_or_404(Post, id = id)
+    post.delete()
+
+    return HttpResponseRedirect('/posts/index/')
