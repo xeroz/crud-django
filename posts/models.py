961a3a989a24a853442908f9131f7541b166a025
juan <chavezvasquezjuan@gmail.com>
2017-04-30 20:30:39 -0500

    fixes

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    
+
     title     = models.CharField(max_length = 120)
     content   = models.TextField()
     updated   = models.DateTimeField(auto_now = True, auto_now_add = False)
