169a2c9d3556e989165f2e2ed05f98c122609f3f
juan <chavezvasquezjuan@gmail.com>
2017-10-22 12:14:09 -0500

    custom command seed

--- posts/models.py
+++ posts/models.py
@@ -5,9 +5,9 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
 
-    title     = models.CharField(max_length = 120)
-    content   = models.TextField()
-    updated   = models.DateTimeField(auto_now = True, auto_now_add = False)
+    title = models.CharField(max_length = 120)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now = True, auto_now_add = False)
     timestamp = models.DateTimeField(auto_now = False, auto_now_add = True)
 
     def __unicode__(self):
--- posts/views.py
+++ posts/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
+
+
 def posts_home(request):
     return HttpResponse("<h1>sdfd</h1>")
+
 
 def post_list(request):
     posts = Post.objects.all()
@@ -16,15 +19,16 @@ def post_list(request):
 
     return render(request, 'post/index.html', context)
 
+
 def post_create(request):
 
     if request.method == 'POST':
-        title   = request.POST["title"]
+        title = request.POST["title"]
         content = request.POST["content"]
 
         post = Post(
-            title   = title,
-            content = content
+            title=title,
+            content=content
         )
         post.save()
 
@@ -36,15 +40,16 @@ def post_create(request):
 
     return render(request, 'post/create.html', context)
 
+
 def post_edit(request, id):
-    
-    post = get_object_or_404(Post, id = id)
-    
+
+    post = get_object_or_404(Post, id=id)
+
     if request.method == 'POST':
-        title   = request.POST["title"]
+        title = request.POST["title"]
         content = request.POST["content"]
-        
-        post.title   = title
+
+        post.title = title
         post.content = content
         post.save()
 
@@ -56,9 +61,10 @@ def post_edit(request, id):
 
     return render(request, 'post/edit.html', context)
 
+
 def post_delete(render, id):
-    
-    post = get_object_or_404(Post, id = id)
+
+    post = get_object_or_404(Post, id=id)
     post.delete()
 
     return HttpResponseRedirect('/posts/index/')
